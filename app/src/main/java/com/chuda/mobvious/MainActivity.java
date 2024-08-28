0bffb9eec8ba9362caea6a1904fa493aa97da504
Choirul Huda <choirulhuda07@gmail.com>
2024-08-28 14:51:05 +0700

    Commit Pertama

---
+++ app/src/main/java/com/chuda/mobvious/MainActivity.java
@@ -0,0 +1,35 @@
+package com.chuda.mobvious;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtEmail, edtPassword;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                Toast.makeText(getApplicationContext(), "Email saya: "+email+"\nPassword : "+password, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
